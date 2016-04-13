2040410af9d751748ab2f71f3c697548db63a005
Paolo Ricca <p.ricca-cons@almaviva.it>
2016-04-13 15:16:28 +0200

    test

---
+++ experiences.js
@@ -0,0 +1,325 @@
+﻿var esperienze = [
+    {
+        "anno": "'97",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Seguo con interesse la creazione delle immagini per interni, ideando ed impaginando contenuti rivolti principalmente al settore della ristorazione. Ristoranti, Bar, Pizzerie, l'adozione degli editor grafici più avanzati mi consente di approfondire alcuni aspetti dell'elaborazione digitale applicata al food presentation.",
+        "cliente": "Aquarium Service",
+        "tecnologie": [ 
+            { 
+                "text": "Adobe Photoshop"
+            },
+            {
+                "text": "Adobe Illustrator"
+            },
+            {
+                "text": "Quark XPress"
+            }
+        ],
+        "quot": "Un'esperienza che ha contribuito a consolidare nozioni grafiche fino ad allora solo teoriche, fondamentale."
+    },
+    {
+        "anno": "'98",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Periodici, riviste e settimanali, l'editoria mi consente di esprimere idee sempre nuove ed in continuo fermento. Lavoro a stretto contatto con esperti della comunicazione e del design curando personalmente il concept, l'impaginazione e la stampa di una brochure dedicata ad uno degli eventi più importanti del secolo scorso, il Giubileo.",
+        "cliente": "Graffio Group",
+        "tecnologie": [
+            {
+                "text": "Adobe Photoshop"
+            },
+            {
+                "text": "Adobe Illustrator"
+            },
+            {
+                "text": "Quark XPress"
+            }
+        ],
+        "quot": "Un percorso impegnativo e gratificante che mi ha dato la possibilità di seguire l'intero flusso di un vero progetto editoriale."
+    },
+    {
+        "anno": "'99",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Pellicole ed impianti di stampa, seguo da vicino tutti i passi che contribuiscono a finalizzare l'ultima fase della creatività. Lo spazio per inventare è poco, in questo contesto essere propositivi ed efficaci risulta difficile, ma ho imparato che la buona organizzazione spesso è determinante per l'incremento della produttività.",
+        "cliente": "Stamperia Lampo",
+        "tecnologie": [
+            {
+                "text": "Adobe Illustrator"
+            },
+            {
+                "text": "Quark XPress"
+            }
+        ],
+        "quot": "Qui ho scoperto l'arte e la magia della stampa tradizionale, il profumo intenso degli inchiostri e della carta."
+    },
+    {
+        "anno": "'02",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Il web mi appassiona ma la forma non basta, mi rendo conto che internet viaggia sempre più velocemente e la gestione informatizzata dei dati risulta ormai determinante per gestire a 360° il flusso di produzione. Entro in contatto con una solida realtà aziendale che mi consente di apprendere i fondamenti dello sviluppo software.",
+        "cliente": "Servizi d'Azienda",
+        "tecnologie": [
+            {
+                "text": "Logica della programmazione"
+            },
+            {
+                "text": "Visual Basic for applications"
+            },
+            {
+                "text": "ASP 3.0 (Active Server Pages)"
+            }
+        ],
+        "quot": "Partecipo con entusiasmo a svariati corsi di formazione ed acquisisco nozioni fondamentali che integrano dinamicità operativa ai miei progetti."
+    },
+    {
+        "anno": "'03",
+        "titolo": null,
+        "image": "",
+        "descrizione": "In questo periodo sviluppo diverse procedure automatizzate che si connettono al server centrale della Camera dei Deputati, l'obiettivo è quello di strutturare nel formato nativo XML le informazioni richieste su alcune sezioni del sito istituzionale, visualizzando i dati in modo più efficiente e dinamico rispetto al tradizionale HTML.",
+        "cliente": "Camera dei Deputati",
+        "tecnologie": [
+            {
+                "text": "Visual Basic 6"
+            },
+            {
+                "text": "Oracle 9.i"
+            },
+        ],
+        "quot": "Lavoro a stretto contatto con molteplici seniority dello sviluppo maturando un know-how importante nell'acquisizione ed ottimizzazione delle risorse."
+    },
+    {
+        "anno": "'05",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Sviluppo una web application che consente di catalogare le estensioni dei file registrati sui server della Camera dei Deputati. Implemento le interfacce utente, ma l'attività più importante riguarda lo studio del Query Language per l'estrazione e la manipolazione dei dati, scopro da vicino il modello entità-relazione per la progettazione dei Database.",
+        "cliente": "Camera dei Deputati",
+        "tecnologie": [
+            {
+                "text": "Net Framework 1.1"
+            },
+            {
+                "text": "Visual Studio 2003"
+            },
+            {
+                "text": "ASP.Net"
+            },
+            {
+                "text": "Oracle 9.i"
+            }
+        ],
+        "quot": "La logica della programmazione è fondamentale, ma lo studio dei Database relazionali apre le porte al consolidamento del mio percorso didattico."
+    },
+    {
+        "anno": "'07",
+        "titolo": null,
+        "image": "",
+        "descrizione": "SEGUIMI. Collaboro attivamente per informatizzare le fasi della spedizione che da un vecchio recapito inoltrano la posta ordinaria presso una nuova destinazione geografica. Un'esperienza fortemente indirizzata all'organizzazione strutturata del codice che mi consente di approfondire la programmazione orientata agli oggetti.",
+        "cliente": "Poste italiane",
+        "tecnologie": [
+            {
+                "text": "Net Framework 2.0"
+            },
+            {
+                "text": "Visual Studio 2005"
+            },
+            {
+                "text": "ASP.Net"
+            },
+            {
+                "text": "Javascript"
+            },
+            {
+                "text": "SQL Server 2005"
+            }
+        ],
+        "quot": "Un contesto operativo dinamico dove occorre prestare particolare attenzione all'ottimizzazione del codice."
+    },
+    {
+        "anno": "'08",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Per la prima volta mi trovo a reingegnerizzazione un'applicazione esistente, Vetrina Bancoposta, scritta in ASP 3.0 e già in produzione. Rendere lo strato applicativo più robusto ed efficiente, l'obiettivo è quello di adottare il Framework .NET ed implementare il nuovo prototipo senza alterarne i flussi e le funzionalità esistenti.",
+        "cliente": "Poste italiane",
+        "tecnologie": [
+            {
+                "text": "Net Framework 2.0"
+            },
+            {
+                "text": "Visual Studio 2005"
+            },
+            {
+                "text": "Asp.Net"
+            },
+            {
+                "text": "Javascript"
+            },
+        ],
+        "quot": "Aggiorno lo strato applicativo sostituendo i vecchi oggetti con quelli più evoluti messi a disposizione dalla nuova piattaforma Microsoft NET."
+    },
+    {
+        "anno": "'09",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Parallelamente allo sviluppo dell'ERP Posta Packaging, in ambiente SAP, il cliente ha l'esigenza di pubblicare sul web un punto di accesso centralizzato per la visualizzazione e la stampa dei report giornalieri. Progetto e sviluppo il portale, implemento il prototipo funzionale e le classi di codice business, Front e Back-End.",
+        "cliente": "Poste italiane",
+        "tecnologie": [
+            {
+                "text": "Net Framework 2.0"
+            },
+            {
+                "text": "Visual Studio 2005"
+            },
+            {
+                "text": "Asp.Net"
+            },
+            {
+                "text": "SAP Connector"
+            },
+            {
+                "text": "Web Services"
+            },
+            {
+                "text": "CSS 2.0"
+            },
+            {
+                "text": "Javascript"
+            },
+        ],
+        "quot": "Mi connetto alla piattaforma SAP via Web Services e pubblico le informazioni ricevute implementando fogli di stile appositamente dedicati al report."
+    },
+    {
+        "anno": "'10",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Parallelamente allo sviluppo dell'ERP Posta Packaging, in ambiente SAP, il cliente ha l'esigenza di pubblicare sul web un modulo applicativo per la centralizzazione delle notifiche relative ai pacchi smarriti. Sviluppo una classe di codice business che consente di registrare un plico leggendo l'input direttamente da un lettore di codice a barre.",
+        "cliente": "Poste Italiane",
+        "tecnologie": [
+            {
+                "text": "Net Framework 2.0"
+            },
+            {
+                "text": "Visual Studio 2005"
+            },
+            {
+                "text": "Asp.Net"
+            },
+            {
+                "text": "SAP Connector"
+            },
+            {
+                "text": "Web Services"
+            },
+            {
+                "text": "Ajax Library"
+            },
+        ],
+        "quot": "Interagisco con il gruppo di lavoro e contribuisco all'installazione della componente applicativa presso il centro operativo dell'ufficio postale."
+    },
+    {
+        "anno": "'11",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Rivolto principalmente al settore privato, il modulo web che sviluppo consente la gestione informatizzata dell’Albo Fornitori, attraverso un flusso che inizia con l'invito alla registrazione per arrivare alla completa qualificazione delle Imprese. Implemento funzionalità dedicate al vendor rating e alla valutazione delle performance.",
+        "cliente": "Procurement Services",
+        "tecnologie": [
+            {
+                "text": "Net Framework 3.5"
+            },
+            {
+                "text": "Visual Studio 2008"
+            },
+            {
+                "text": "SQL Server 2008"
+            },
+            {
+                "text": "CA ERwin Data Modeler"
+            },
+            {
+                "text": "ASP.Net"
+            },
+            {
+                "text": "Ajax Library"
+            },
+            {
+                "text": "iTextSharp PDF"
+            }
+        ],
+        "quot": "Data l'alta configurabilità richiesta, in questo scenario ho sviluppato un motore per la generazione automatizzata di Web Form."
+    },
+    {
+        "anno": "'12",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Rivolto al settore privato, il modulo web che sviluppo consente la gestione personalizzata di un evento negoziale, in modalità Asta o Gara, attraverso un flusso che inizia con la configurazione per arrivare alla sua completa aggiudicazione finale. Implemento numerose funzionalità dedicate alla valutazione dei concorrenti.",
+        "cliente": "Procurement Services",
+        "tecnologie": [
+            {
+                "text": "Net Framework 3.5"
+            },
+            {
+                "text": "Visual Studio 2008"
+            },
+            {
+                "text": "SQL Server 2008"
+            },
+            {
+                "text": "CA ERwin Data Modeler"
+            },
+            {
+                "text": "ASP.Net"
+            },
+            {
+                "text": "Ajax Library"
+            },
+            {
+                "text": "iTextSharp PDF"
+            }
+        ],
+        "quot": "Data l’alta interattività richiesta, è stato necessario sviluppare un temporizzatore automatico per verificare in tempo reale la scadenza dell’evento."
+    },
+    {
+        "anno": "'14",
+        "titolo": null,
+        "image": "",
+        "descrizione": "Disegno e sviluppo prodotti software rivolti a diversi canali del settore IT, curando personalmente tutti i processi coinvolti nell'intero ciclo di vita del progetto. Siti internet, Sistemi informativi, Portali e Intranet aziendali, oggi sto lavorando ad una piattaforma web che consente ad Enti ed Imprese di sfruttare le nuove potenzialità del MEPA.",
+        "cliente": "Mercato elettronico",
+        "tecnologie": [
+            {
+                "text": "Net Framework 3.5"
+            },
+            {
+                "text": "Visual Studio 2008"
+            },
+            {
+                "text": "SQL Server 2008"
+            },
+            {
+                "text": "CA ERwin Data Modeler"
+            },
+            {
+                "text": "ASP.Net"
+            },
+            {
+                "text": "ASP.Net MVC"
+            },
+            {
+                "text": "REST Services"
+            },
+            {
+                "text": "ASP.Net Web API"
+            },
+            {
+                "text": "JSON"
+            },
+            {
+                "text": "Bootstrap"
+            },
+            {
+                "text": "JQuery, BackBone.js"
+            }
+        ],
+        "quot": "Basket, Ordini di Acquisto, Gare telematiche e un catalogo configurabile in base alle proprie esigenze, questi sono i plus che sto sviluppando, <b>web a 360°</b>."
+    },
+
+]
